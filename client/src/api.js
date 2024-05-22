bed0536966dcd0b47af5db9d26ad375579e62552
coderGtm <gautammehta0000@gmail.com>
2024-05-22 23:05:28 +0530

    cleaned project to rewrite from scratch

--- client/src/api.js
+++
@@ -1,7 +0,0 @@
-// src/api/api.js
-import axios from 'axios';
-
-const API = axios.create({ baseURL: '/api' });
-
-export const registerUser = (userData) => API.post('/users/register', userData);
-export const loginUser = (userData) => API.post('/users/login', userData);
